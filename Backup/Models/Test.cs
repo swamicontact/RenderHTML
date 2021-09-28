995c7fab5c8d1068cc5079bf6af6f842981a41b3
swami <srama3@acehardware.com>
2021-09-28 13:58:55 +0530

    New Folder Added

---
+++ Backup/Models/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RenderHTMLinView.Models
+{
+    public class Test
+    {
+        public string HTMLContent { get; set; }
+    }
+}
